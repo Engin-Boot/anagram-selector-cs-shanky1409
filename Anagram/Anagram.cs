eff4d2a373f7c85c461f489668774ec36afa4e3b
Dixit <Shashank.Dixit@philips.com>
2020-09-07 00:19:37 +0530

    added implementation

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml.Schema;
 
 namespace Anagram
 {
     public class AnagramSelector
     {
         public bool WordPairIsAnagram(string word1, string word2) {
-            //Insert the correct implementation here
+            if(word1.Length==word2.Length)
+            {
+                var word1arr = word1.ToCharArray();
+                var word2arr = word2.ToCharArray();
+                Array.Sort(word1arr);
+                Array.Sort(word2arr);
+                if(new String(word1arr)== new String(word2arr))
+                    return true;   
+            }
             return false;
         }
         public List<string> SelectAnagrams(string word, List<string> candidates) {
-            //Insert the correct implementation here
-            return candidates;
+            var anagrams = new List<string>();
+            foreach(var word2 in candidates)
+            {
+                if(WordPairIsAnagram(word, word2))
+                    anagrams.Add(word2);
+            }
+            return anagrams;
         }
     }
 }
